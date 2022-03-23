70255c4f40ff5c2f98c63aca0c297fde416d2df2
Fathur Rahman <fathur.rahman@udana.id>
2022-03-24 00:54:50 +0700

    integrasi project baru

--- pages/dashboard/projects/create.vue
+++ pages/dashboard/projects/create.vue
@@ -16,12 +16,12 @@
           <h3 class="text-2xl text-gray-900 mb-4">Create New Projects</h3>
         </div>
         <div class="w-1/4 text-right">
-          <a
-            href="/dashboard/detail.html"
+          <button
+            @click="save"
             class="bg-green-button hover:bg-green-button text-white font-bold px-4 py-1 rounded inline-flex items-center"
           >
             Save
-          </a>
+          </button>
         </div>
       </div>
       <div class="block mb-2">
@@ -41,6 +41,7 @@
                     class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
                     type="text"
                     placeholder="Contoh: Demi Gunpla Demi Istri"
+                    v-model="campaign.name"
                   />
                 </div>
                 <div class="w-full md:w-1/2 px-3">
@@ -53,6 +54,7 @@
                     class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
                     type="number"
                     placeholder="Contoh: 200000"
+                    v-model.number="campaign.goal_amount"
                   />
                 </div>
                 <div class="w-full px-3">
@@ -65,6 +67,7 @@
                     class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
                     type="text"
                     placeholder="Deskripsi singkat mengenai projectmu"
+                    v-model="campaign.short_description"
                   />
                 </div>
                 <div class="w-full px-3">
@@ -77,6 +80,7 @@
                     class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
                     type="text"
                     placeholder="Contoh: Ayam, Nasi Goreng, Piring"
+                    v-model="campaign.perks"
                   />
                 </div>
                 <div class="w-full px-3">
@@ -89,6 +93,7 @@
                     class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
                     type="text"
                     placeholder="Isi deskripsi panjang untuk projectmu"
+                    v-model="campaign.description"
                   ></textarea>
                 </div>
               </div>
@@ -102,3 +107,138 @@
     <Footer />
   </div>
 </template>
+
+<script>
+export default {
+  middleware: 'auth',
+  data() {
+    return {
+      campaign: {
+        name: '',
+        short_description: '',
+        description: '',
+        goal_amount: 0,
+        perks: ''
+      }
+    }
+  },
+  methods: {
+    async save() {
+      try {
+        let response = await this.$axios.$post(
+          '/api/v1/campaigns',
+          this.campaign
+        )
+        this.$router.push({
+          name: 'dashboard-projects-id',
+          params: { id: response.data.id }
+        })
+        console.log(response)
+      } catch (err) {
+        console.log(err)
+      }
+    }
+  }
+}
+</script>
+
+<style>
+.dashboard-header {
+  background-image: url('/auth-background.svg');
+  background-position: top right;
+  background-repeat: no-repeat;
+  background-color: #3b41e3;
+  background-size: 250px;
+  height: 100px;
+}
+
+.cta-clip {
+  position: relative;
+  top: 200px;
+  bottom: 0;
+  right: 0;
+  left: 0;
+  width: 100%;
+  height: 300px;
+  background-position: top right;
+  background-size: 300px;
+  background-repeat: no-repeat;
+  background-color: #fff;
+  transform: skewY(4deg);
+  transform-origin: bottom right;
+}
+
+.call-to-action {
+  background-image: url('/auth-background.svg');
+  background-position: top right;
+  background-repeat: no-repeat;
+  background-size: 450px;
+}
+
+.card-project {
+  transition: all 0.3s ease 0s, opacity 0.5s cubic-bezier(0.5, 0, 0, 1) 1ms;
+  max-height: 445px;
+  overflow: hidden;
+
+  .button-cta {
+    opacity: 0;
+    transition: all 300ms ease;
+  }
+
+  &:hover {
+    box-shadow: 0 4px 25px 0 rgba(0, 0, 0, 0.15);
+
+    .button-cta {
+      opacity: 1;
+      transition: all 300ms ease;
+    }
+
+    .progress-bar,
+    .progress-info {
+      opacity: 0;
+      height: 0px;
+      margin: 0px;
+      padding: 0px;
+      transition: all 300ms ease;
+    }
+  }
+}
+
+footer {
+  z-index: inherit;
+}
+
+.hero-underline {
+  text-decoration-color: #1abc9c;
+}
+
+.testimonial-user {
+  opacity: 0.4;
+  &.active {
+    opacity: 1;
+    border: 5px solid #fff;
+    box-shadow: 0 0 0 1px #3b41e3;
+  }
+}
+
+.list-check {
+  li {
+    background: url('/icon-checklist.svg') no-repeat left 8px;
+    padding: 6px 0px 3px 28px;
+  }
+}
+
+.item-thumbnail:hover {
+  background-color: #ff872e;
+  border-radius: 20px;
+  &:after {
+    position: absolute;
+    top: 38%;
+    left: 41%;
+    content: url('/icon-thumbnail-hover.svg');
+  }
+  img {
+    opacity: 0.3;
+  }
+}
+</style>
